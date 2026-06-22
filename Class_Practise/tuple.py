edeedc4a555cac649616def0b27aebcdefc5f9d7
Niksin <itsnikhil0786@gmail.com>
2026-06-22 15:02:35 +0530

    update

---
+++ Class_Practise/tuple.py
@@ -0,0 +1,31 @@
+#-------Defination and property of tuple----------------#
+#Tuple is a data structure in python used to store multiple data of different types with comma(,) in round braces.
+#2.Immutable
+#3. Support indexing slicing and ordered.
+# 
+# #--------Creation of tuple----------------#
+# t1,t2,t3=(50,40,"nik")
+# print(type(t1)
+# print(t1)
+# print(t2)
+# print(t3)
+
+# marks_tuple=(50,55,69,34,89)
+# print(marks_tuple[-1])
+# print(marks_tuple[::-1])
+
+#mutability of tuple
+# marks_tuple=(50,55,69,34,89)
+# marks_tuple[2]=500
+# print(marks_tuple)
+
+#Traversing
+# marks_tuple=(50,55,69,34,89)
+# def marks_tuples(m):
+#     new_tuple=[]
+#     for i in marks_tuple:
+#         if i>55:
+#             new_tuple.append(i)
+#     return new_tuple
+# res=marks_tuples(marks_tuple)
+# print(res)
